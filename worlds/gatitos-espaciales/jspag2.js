183f0f98bd27d16017ceff7a4dd2fda07c4f2d24
jesus <facundofreijomil@outlook.com>
2022-07-18 10:53:55 -0300

    mili

---
+++ worlds/gatitos-espaciales/jspag2.js
@@ -0,0 +1,51 @@
+function OpenModal() {
+    let element = document.getElementById('overlay')
+    element.style.display = 'block'
+  }
+  function CloseModal() {
+    let element = document.getElementById('overlay')
+    element.style.display = 'none'
+  }
+
+
+  function OpenModal2() {
+    let element = document.getElementById('overlay2')
+    element.style.display = 'block'
+  }
+  function CloseModal2() {
+    let element = document.getElementById('overlay2')
+    element.style.display = 'none'
+  }
+
+  
+
+  function OpenModal3() {
+    let element = document.getElementById('overlay3')
+    element.style.display = 'block'
+  }
+  function CloseModal3() {
+    let element = document.getElementById('overlay3')
+    element.style.display = 'none'
+  }
+
+
+
+  function OpenModal4() {
+    let element = document.getElementById('overlay4')
+    element.style.display = 'block'
+  }
+  function CloseModal4() {
+    let element = document.getElementById('overlay4')
+    element.style.display = 'none'
+  }
+
+
+
+  function OpenModal5() {
+    let element = document.getElementById('overlay5')
+    element.style.display = 'block'
+  }
+  function CloseModal5() {
+    let element = document.getElementById('overlay5')
+    element.style.display = 'none'
+  }
