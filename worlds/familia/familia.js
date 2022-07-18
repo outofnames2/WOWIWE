bed754cbfe21cd5f50766f424b38d2088f0ddfd6
jesus <facundofreijomil@outlook.com>
2022-07-18 10:56:47 -0300

    jesus(facundo)

---
+++ worlds/familia/familia.js
@@ -0,0 +1,6 @@
+import {fakeLink, getSavedWorlds} from '/dataWorld.js';
+
+let worlds = getSavedWorlds();
+const link = document.querySelector(".url");
+console.log(worlds);
+link.addEventListener("click", event=>fakeLink(link, worlds, event), {once: true});
